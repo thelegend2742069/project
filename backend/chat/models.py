08e9ec59e4320acff44dd18a0f742fa0d5d604ed
Pranav Shirole <blab8650@gmail.com>
2025-05-06 10:43:47 +0530

    created room and chat message model

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from api.models import Room
 
-# Create your models here.
+
+class Message(models.Model):
+    text = models.CharField(max_length=100)
+    room_code = models.ForeignKey(Room, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
