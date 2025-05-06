38fc21a617ad0f754ab7bc40dcd90ab3b6024794
Pranav Shirole <blab8650@gmail.com>
2025-05-06 10:41:01 +0530

    created user and added jwt tokens

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -10,3 +10,6 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'password']
         extra_kwargs = {'password': {'write_only': True}}
     
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from api.views import CreateUserView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls')),
-    path('', include('chat.urls')),
+    path('api/user/register/', CreateUserView.as_view(), name='register'),
+    path('api/token/', TokenObtainPairView.as_view(), name='get_token'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh_token'),
+    # path('', include('api.urls')),
+    # path('', include('chat.urls')),
 ]
