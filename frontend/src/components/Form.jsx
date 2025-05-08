53f5d976f47454038b220dadc58555c91c25aa62
Pranav Shirole <blab8650@gmail.com>
2025-05-09 04:16:40 +0530

    added navigation

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react"
 import api from "../api"
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants"
+import { Navigate, useNavigate } from "react-router-dom"
 
 
 function Form({route, method}) {
-    const [username, setUsername] = useState()
-    const [password, setPassword] = useState()
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const navigate = useNavigate()
 
     const name = method === 'login' ? "Login" : "Register"
 
@@ -14,13 +16,16 @@ function Form({route, method}) {
         
         try{
             const res = await api.post(route, {username, password})
+            console.log(res.data)
+            
             if (method === 'login') {
                 localStorage.setItem(ACCESS_TOKEN, res.data.access)
                 localStorage.setItem(REFRESH_TOKEN, res.data.refresh)
-             
+                navigate('/home')
+            } else {
+                navigate('/login')
             }
-            console.log(res.data)
-
+            
         } catch(error) {
             console.log(error)
         }
