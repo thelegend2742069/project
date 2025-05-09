5e2cd7b98f5727ff19cf7d4247cf84bf75a20c40
Pranav Shirole <blab8650@gmail.com>
2025-05-09 22:09:22 +0530

    added route to join room

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 import NotFound from "./pages/NotFound"
+import JoinRoom from "./pages/JoinRoom"
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/room" element={<JoinRoom />} />
         <Route path="*" element={<NotFound />} />        
       </Routes>
     </BrowserRouter>
