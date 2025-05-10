930dcdfe4e9986d27a3bb1fdac21af3881a43802
Pranav Shirole <blab8650@gmail.com>
2025-05-10 20:10:19 +0530

    added media to url patterns

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from api.views import UserCreate, RoomDelete, RoomListCreate
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
@@ -29,3 +31,5 @@ urlpatterns = [
     path('api/room/<str:room_code>/', include('chat.urls')),
     path('api/room/<str:room_code>/', include('video_handler.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
