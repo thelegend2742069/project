752e90a8669277ee589439a35366ff11f2598f17
Pranav Shirole <blab8650@gmail.com>
2025-05-11 18:10:51 +0530

    added handle username on username submit

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -4,7 +4,7 @@ import api from '../api';
 
 
 function Chat() {
-    const isAuthorized = useAuthorizedStatus()
+    const [isAuthorized, setIsAuthorized] = useState(useAuthorizedStatus())
     const [messages, setMessages] = useState([]);
     const [username,setUsername] = useState('')
     const [messageText, setMessageText] = useState('');
@@ -14,19 +14,29 @@ function Chat() {
         setMessages(res.data)
     }
 
-    const handleSend = async () => {
+    const handleUsername = async (e) => {
+        e.preventDefault()
+
+        setIsAuthorized(true)
+        console.log(isAuthorized)
+
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
         try{
             console.log(`sending post request to /api${window.location.pathname}/messages/`)
-            console.log(`data: text=${messageText}, username=${username || "guest"}`)
-            const res = await api.post(`/api${window.location.pathname}/messages/`, {text:messageText, username:username||"guest"})
+            console.log(`data: text=${messageText}, username=${username}`)
+            const res = await api.post(`/api${window.location.pathname}/messages/`, {text:messageText, username:username})
             console.log(res.data)
+            setMessageText('')
+            setMessages(messages => [...messages, res.data])
         } catch (error) {
             console.log(error)
             alert(error)
         }
-        setMessageText('')
     }
-
     useEffect(() => {
         getMessages()
     }, []);
@@ -40,30 +50,34 @@ function Chat() {
             
             { isAuthorized ?
                 <div className="message-box">
-                    <input 
-                    className="message-input" 
-                    type="text" 
-                    value={messageText}
-                    onChange={(e) => setMessageText(e.target.value)}
-                    placeholder='type something'
-                    />
+                    <form onSubmit={handleSubmit} className="message-form">
+                        <input 
+                        className="message-input" 
+                        type="text" 
+                        value={messageText}
+                        onChange={(e) => setMessageText(e.target.value)}
+                        placeholder='type something'
+                        />
 
-                    <button 
-                    className="send-message" 
-                    type='submit'
-                    onClick={handleSend}
-                    >
-                        send
-                    </button>
+                        <button 
+                        className="send-message" 
+                        type='submit'
+                        >
+                            send
+                        </button>
+                    </form>
                 </div> : 
-                <div className="username">
-                    <input 
-                    className="username-input"
-                    type="text"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    placeholder='enter a username'
-                    />
+                <div className="username-box">
+                    <form className="username-form" onSubmit={handleUsername}>
+                        <input 
+                        className="username-input"
+                        type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        placeholder='enter a username'
+                        />
+                        <button className="username-button" type='submit'>ok</button>
+                    </form>
                 </div>
             }
         </div>
